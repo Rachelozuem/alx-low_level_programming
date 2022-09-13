4e9001b5a68cef903f8ed4f5247b82e6ba642d99
Rachelozuem <ozuemrachel@gmail.com>
2022-09-13 15:22:23 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tire.h>
+
+/**
+ * main - Main Entry
+ *
+ * Return: Always Success
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return(0);
+}
