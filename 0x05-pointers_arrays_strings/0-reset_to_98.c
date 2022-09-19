6cf1b7683756ac6f0a1e64ecfd8d6111deccaed8
Rachelozuem <ozuemrachel@gmail.com>
2022-09-19 11:22:10 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,30 +1,12 @@
 include "main.h"
 
 /**
- * print_rev - Entry point
- *
- * Description: 'The function that prints strings in reverse order'
- *
- * @str: The string to br displayed
- * Return: Always 0 (Success)
+ * reset_to_98- updates value of pointer to 98
+ * @n:input to update to 98.
  */
+void reset_to_98(int *n)
+[
+	*n = 98;
+]
 
-void print_rev(char*str)
-{
-	int i;
-
-	i=0;
-	while(*str!= '\0')
-	{
-		i++;
-		str++;
-	}
-	while(i>=0)
-        }
-        _putchar(*str);
-	i--;
-	str--;
-	}
-        _putchar('\n')
-}
 
