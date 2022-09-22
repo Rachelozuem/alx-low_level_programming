16233d484b5dfe40c651a896fa7d24aec378f2d0
Rachelozuem <ozuemrachel@gmail.com>
2022-09-22 14:59:31 -0700

    Task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,5 +19,7 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+void printf_buffer(char *b, int size);
+int _putchar(char c);
  
-#endif /*MAIN_H*/ 
+#endif /*MAIN_H_INCLUDED */ 
