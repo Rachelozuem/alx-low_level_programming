8a9fdb943f321eba44a040420dcb847fb1526795
Rachelozuem <ozuemrachel@gmail.com>
2022-10-04 14:52:06 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array of chars and initializes it with
+ * a specific char.
+ * @size: size of array.
+ * @c: char to initialize array with.
+ *
+ * Return: NULL if it fails or
+ * pointer to array.
+ */
+char *create_array(unsigned int size, cgar c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+        }
+       arr = malloc(sizeof(char) * size);
+       /* check if malloc was successfull */
+       
+       if (arr == NULL)
+               return (NULL);
+
+        for (i = 0; i < size; i++)
+	{
+	       arr[i] = c;
+	}
+        return (arr);
+}	
+                    
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char *_strduo(char *str)
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+
