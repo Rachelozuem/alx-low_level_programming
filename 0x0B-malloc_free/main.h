49b13746fa7e474550163a327267cb344feca63c
Rachelozuem <ozuemrachel@gmail.com>
2022-10-04 14:58:07 -0700

    Task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
 char *_strduo(char *str)
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 
+#endif
